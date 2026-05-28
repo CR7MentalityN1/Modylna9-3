d124b1c50ea5f5ff68bbe6e234b8d6d455f54122
CR7MentalityN1 <dolphi034@gmail.com>
2026-05-28 11:55:49 +0300

    feat: add Appliance model

---
+++ Models/Appliance.cs
@@ -0,0 +1,7 @@
+namespace ModuleWork3.Models;
+
+public class Appliance {
+    public string Category { get; set; }
+    public string Country { get; set; }
+    public decimal Price { get; set; }
+}
